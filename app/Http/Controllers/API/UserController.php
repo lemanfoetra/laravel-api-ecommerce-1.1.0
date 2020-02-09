5b457a2dec6583757e03a7390b733df55e7ed97d
Leman Fotera <lemanfoetra@gmail.com>
2020-02-09 11:37:14 +0700

    siap tempur dan sudah di install passport

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+
+
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('login','register');
+    }
+    
+
+
+    public function index()
+    {
+        return "ok, It Work.";
+    }
+
+
+
+    public function login(){
+        if(Auth::attempt([
+                'email' => request('email'), 
+                'password' => request('password')
+            ])){
+
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('eapi')->accessToken;
+
+            return response()->json(['success' => $success], 200);
+        }
+        else{
+            return response()->json(['error'=>'User Tidak Terdaftar'], 401);
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::prefix('user')->group(function () {
+    Route::get('/','API\UserController@index')->name('user.index');
+    Route::post('login','API\UserController@login')->name('user.login');
+});
+
