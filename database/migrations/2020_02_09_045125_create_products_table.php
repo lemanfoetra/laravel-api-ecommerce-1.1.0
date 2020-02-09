7d5b5aeba53ce2b290bfd16fe589bc54c76fd95a
Leman Fotera <lemanfoetra@gmail.com>
2020-02-09 15:13:19 +0700

    Membuat migration Product dan Review

--- database/migrations/2020_02_09_045125_create_products_table.php
+++ database/migrations/2020_02_09_045125_create_products_table.php
@@ -15,6 +15,12 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unisigned()->index();
+            $table->string('name',255);
+            $table->string('detail');
+            $table->integer('price');
+            $table->integer('discount');
+            $table->integer('stock');
             $table->timestamps();
         });
     }
