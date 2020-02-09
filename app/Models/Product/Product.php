5ebc080cec6e81a0754db6b7d5437f293dc66036
Leman Fotera <lemanfoetra@gmail.com>
2020-02-09 21:16:38 +0700

    Membuat Relasi Eloquent Antara Review -> Product

--- app/Models/Product/Product.php
+++ app/Models/Product/Product.php
@@ -2,9 +2,15 @@
 
 namespace App\Models\Product;
 
+use App\Models\Product\Review;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    
+
+    public function reviews()
+    {
+        $this->hasMany(Review::class);
+    }
 }
--- app/Models/Product/Review.php
+++ app/Models/Product/Review.php
@@ -3,8 +3,13 @@
 namespace App\Models\Product;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Product\Product;
 
 class Review extends Model
 {
-    //
+    
+    public function product()
+    {
+        $this->belongsTo(Product::class);
+    }
 }
